0d80bc7c2beada050ab3b123573538e995b2f801
Ekyn Nicholl <102761477@students.swinburne.edu.my>
2023-10-23 17:57:58 +0800

    updated create event for mobile friendly

--- app/(dashboard)/homepage/page.tsx
+++ app/(dashboard)/homepage/page.tsx
@@ -1406,7 +1406,7 @@ export default function Homepage() {
 									Create Event
 								</h3>
 
-								<hr className="border-t-2 border-slate-200 my-4 w-[285px] lg:w-[505px]" />
+								<hr className="border-t-2 border-slate-200 my-4 w-[270px] lg:w-[505px]" />
 
 								<div>
 									<p className="text-[12px] lg:text-[14px] text-mb-7 mb-[2px] font-normal text-slate-500 ml-[2px]">
@@ -1523,7 +1523,7 @@ export default function Homepage() {
 										{detail.event_names.map((event_name, eventNameIndex) => (
 											<div key={eventNameIndex}>
 												<p className="text-[12px] lg:text-[14px] text-mb-7 mb-[2px] font-normal text-slate-500 mt-1 ml-[2px]">
-													Event Name
+													Sub-event Name
 													<span className="text-[12px] lg:text-[14px] text-red-500 ml-[2px]">*</span>
 												</p>
 												<input
@@ -1679,7 +1679,7 @@ export default function Homepage() {
 									</div>
 								))}
 
-								<div className="absolute bottom-0 left-0 right-0 p-4 bg-white flex justify-center gap-[2px]">
+								<div className="lg:absolute bottom-0 left-0 right-0 p-4 bg-white flex justify-center gap-[2px]">
 									<button type="button" onClick={addEventDetails} className="rounded-lg px-[7px] py-[5px] lg:px-[10px] lg:py-[5px] border border-slate-800 hover:bg-slate-100 mr-4 text-[12px] lg:text-[15px] focus:shadow-outline focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-slate-900 font-medium">
 										Add Sub-Event
 									</button>
--- components/CreateEvent_Modal.tsx
+++ components/CreateEvent_Modal.tsx
@@ -11,10 +11,10 @@ const Modal: React.FC<ModalProps> = ({ isVisible, onClose, children }) => {
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center z-[999]" id="wrapper">
-      <div className="w-[400px] lg:w-[531px] flex flex-col relative p-10 lg:p-0">
+      <div className="w-[372px] lg:w-[531px] flex flex-col relative p-10 lg:p-0">
         <div>
-          <button className="text-slate-500 text-md md:text-lg lg:text-xl absolute top-1 right-3 hover:font-medium hidden lg:inline mr-3" onClick={() => onClose()}>x</button>
-          <div className="bg-white p-2 rounded-lg border border-slate-200 shadow-md max-h-[90vh] overflow-y-auto">
+          <button className="text-slate-500 text-md md:text-lg lg:text-xl absolute top-1 right-3 hover:font-medium lg:inline mt-10 lg:mt-0 mr-10 lg:mr-3" onClick={() => onClose()}>x</button>
+          <div className="bg-white p-2 rounded-lg border border-slate-200 shadow-md h-[550px] lg:h-[670px] lg:max-h-[90vh] overflow-y-auto">
             {children}
           </div>
         </div>
