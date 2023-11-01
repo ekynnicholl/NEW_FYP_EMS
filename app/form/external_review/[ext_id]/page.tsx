0bbd0f41ed29f9aeed5852324afe97f8c8c0433a
Pinkslushie <110762227+Pinkslushie@users.noreply.github.com>
2023-11-01 17:10:36 +0800

    Updates
    - Fixed PDF printing problem.

--- app/form/external_review/[ext_id]/page.tsx
+++ app/form/external_review/[ext_id]/page.tsx
@@ -14,60 +14,63 @@ export default async function ExternalFormPage({ params }: { params: { ext_id: s
     console.log(data?.[0]);
 
     return (
-        <div className="mx-auto max-w-6xl px-8 my-8 mt-6 mb-[200px]">
-            <div className="ml-10">
-                <div className="flex ml-[13px]">
-                    <div>
-                        <Image
-                            src="/swinburne_logo.png"
-                            alt=""
-                            width={200}
-                            height={300}
-                        />
-                    </div>
-                    <div className="ml-8 mt-2">
-                        <p className="font-medium">Human Resources</p>
-                        <h1 className="text-3xl font-bold text-slate-800 mb-4 mt-4 -ml-[1px]">
-                            Nomination / Travelling Application Form
-                        </h1>
-                    </div>
-                </div>
+        // <div className="mx-auto max-w-6xl px-8 my-8 mt-6 mb-[200px]">
+        // <div className="ml-10">
+        //     <div className="flex ml-[13px]">
+        //         <div>
+        //             <Image
+        //                 src="/swinburne_logo.png"
+        //                 alt=""
+        //                 width={200}
+        //                 height={300}
+        //             />
+        //         </div>
+        //         <div className="ml-8 mt-2">
+        //             <p className="font-medium">Human Resources</p>
+        //             <h1 className="text-3xl font-bold text-slate-800 mb-4 mt-4 -ml-[1px]">
+        //                 Nomination / Travelling Application Form
+        //             </h1>
+        //         </div>
+        //     </div>
 
-                <div className="mb-4 text-slate-800 mt-2">
-                    <p className="mb-2">
-                        <span className="text-[12px] lg:text-[14px] text-red-500 ml-[2px] mr-[6px]">
-                            *
-                        </span>
-                        <span>
-                            Before completing this form, please refer to the separate
-                            document on “General Instructions for completing Nomination /
-                            Travelling Application Form”, which is available on
-                            SharePoint.
-                        </span>
-                    </p>
-                    <p className="mb-2">
-                        <span className="text-[12px] lg:text-[14px] text-red-500 ml-[2px] mr-[6px]">
-                            *
-                        </span>
-                        <span>
-                            All fields are mandatory to complete as required for each
-                            applicable section.
-                        </span>
-                    </p>
-                    <p>
-                        <span className="text-[12px] lg:text-[14px] text-red-500 ml-[2px] mr-[6px]">
-                            *
-                        </span>
-                        <span>
-                            This form is also to be used for any contracted individual as
-                            consultant, and is to be completed where applicable.
-                        </span>
-                    </p>
-                </div>
-            </div>
+        //     <div className="mb-4 text-slate-800 mt-2">
+        //         <p className="mb-2">
+        //             <span className="text-[12px] lg:text-[14px] text-red-500 ml-[2px] mr-[6px]">
+        //                 *
+        //             </span>
+        //             <span>
+        //                 Before completing this form, please refer to the separate
+        //                 document on “General Instructions for completing Nomination /
+        //                 Travelling Application Form”, which is available on
+        //                 SharePoint.
+        //             </span>
+        //         </p>
+        //         <p className="mb-2">
+        //             <span className="text-[12px] lg:text-[14px] text-red-500 ml-[2px] mr-[6px]">
+        //                 *
+        //             </span>
+        //             <span>
+        //                 All fields are mandatory to complete as required for each
+        //                 applicable section.
+        //             </span>
+        //         </p>
+        //         <p>
+        //             <span className="text-[12px] lg:text-[14px] text-red-500 ml-[2px] mr-[6px]">
+        //                 *
+        //             </span>
+        //             <span>
+        //                 This form is also to be used for any contracted individual as
+        //                 consultant, and is to be completed where applicable.
+        //             </span>
+        //         </p>
+        //     </div>
+        // </div>
 
-            <hr className="mt-8" />
+        // <hr className="mt-8" />
 
+        //     <AdminExternalForm data={data?.[0]} />
+        // </div>
+        <div>
             <AdminExternalForm data={data?.[0]} />
         </div>
     );
