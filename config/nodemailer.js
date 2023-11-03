80a16d2f5bfc7967ec58c455d900188951b04479
Pinkslushie <110762227+Pinkslushie@users.noreply.github.com>
2023-11-03 09:23:19 +0800

    Updates
    - Updated the email transport slightly.

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -2,6 +2,7 @@ import nodemailer from "nodemailer";
 
 export const transporter = nodemailer.createTransport({
     service: "gmail",
+    // Login credentials for the gmail account,
     auth: {
         user: 'fypemsmaster369@gmail.com',
         pass: 'lxsw qfhm bzev nstm'
@@ -9,6 +10,7 @@ export const transporter = nodemailer.createTransport({
 });
 
 export const mailOptions = {
-    from: 'fypemsmaster369@gmail.com',
+    from: 'acad.admin_servicedesk <fypemsmaster369@gmail.com>',
+    // Change this to the AAO service desk,
     to: 'fypemsmaster369@gmail.com',
 }
