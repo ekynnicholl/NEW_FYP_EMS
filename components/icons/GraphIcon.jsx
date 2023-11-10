fff67631ca79488ebb0bf86facdb7a729aa0abdc
Winston <60646400+moreyball@users.noreply.github.com>
2023-11-10 21:05:34 +0800

    fix dom property error

--- components/icons/GraphIcon.jsx
+++ components/icons/GraphIcon.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const GraphIcon = () => (
     <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M4 5V19C4 19.5523 4.44772 20 5 20H19" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-        <path d="M18 9L13 13.9999L10.5 11.4998L7 14.9998" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+        <path d="M4 5V19C4 19.5523 4.44772 20 5 20H19" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+        <path d="M18 9L13 13.9999L10.5 11.4998L7 14.9998" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
     </svg>
 );
 
--- components/icons/LightIcon.jsx
+++ components/icons/LightIcon.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const LightIcon = ({ onClick }) => (
     <svg width="23px" height="23px" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg" onClick={onClick} className="inline-block">
-        <g clip-path="url(#a)" stroke="#000000" stroke-width="1.5" stroke-miterlimit="10">
-            <path d="M5 12H1M23 12h-4M7.05 7.05 4.222 4.222M19.778 19.778 16.95 16.95M7.05 16.95l-2.828 2.828M19.778 4.222 16.95 7.05" stroke-linecap="round" />
+        <g clip-path="url(#a)" stroke="#000000" strokeWidth="1.5" stroke-miterlimit="10">
+            <path d="M5 12H1M23 12h-4M7.05 7.05 4.222 4.222M19.778 19.778 16.95 16.95M7.05 16.95l-2.828 2.828M19.778 4.222 16.95 7.05" strokeLinecap="round" />
             <path d="M12 16a4 4 0 1 0 0-8 4 4 0 0 0 0 8Z" fill="#000000" fill-opacity=".16" />
-            <path d="M12 19v4M12 1v4" stroke-linecap="round" />
+            <path d="M12 19v4M12 1v4" strokeLinecap="round" />
         </g>
         <defs>
             <clipPath id="a">
