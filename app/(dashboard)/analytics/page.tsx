74dc5faefc0a4420748b53f4e6949270b6b1a3c1
Pinkslushie <110762227+Pinkslushie@users.noreply.github.com>
2023-11-13 19:56:17 +0800

    Updates
    - Added expenditure/ faculty visualization.

--- app/(dashboard)/analytics/page.tsx
+++ app/(dashboard)/analytics/page.tsx
@@ -2,10 +2,10 @@
 
 import BarChart from "@/components/analytics/BarChart";
 import Histogram from "@/components/analytics/Histogram";
-import LineGraphExpenditure1 from "@/components/analytics/LineGraphExpenditure1";
+import ExpenditureGraph from "@/components/analytics/ExpenditureGraph";
 import BarGraphAttendance from "@/components/analytics/BarGraphAttendance";
 import PieChart from "@/components/analytics/PieChart";
-import { useEffect, useState } from "react";
+import { SetStateAction, useEffect, useState } from "react";
 import cookie from 'js-cookie';
 import { useRouter } from "next/navigation";
 
@@ -61,20 +61,98 @@ export default function Home() {
 		}
 	};
 
+	const [totalGrandTotal, setTotalGrandTotal] = useState<number>(0);
+
+	const handleTotalGrandTotalChange = (total: number) => {
+		setTotalGrandTotal(total);
+	};
+
+	const currentDate = new Date();
+	const currentMonth = String(currentDate.getMonth() + 1).padStart(2, '0');
+	const currentYear = String(currentDate.getFullYear());
+
+	const [selectedMonth, setSelectedMonth] = useState(currentMonth);
+	const [selectedYear, setSelectedYear] = useState(currentYear);
+
+	const handleMonthChangeExpenditure = (event: { target: { value: SetStateAction<string>; }; }) => {
+		setSelectedMonth(event.target.value);
+	};
+
+	const handleYearChangeExpenditure = (event: { target: { value: SetStateAction<string>; }; }) => {
+		setSelectedYear(event.target.value);
+	};
+
+	const getMonthText = (numericMonth: string) => {
+		const months = [
+			'January', 'February', 'March', 'April', 'May', 'June',
+			'July', 'August', 'September', 'October', 'November', 'December'
+		];
+
+		return months[parseInt(numericMonth, 10) - 1];
+	};
+
+	const generateYearOptions = (startYear: number, endYear: number) => {
+		const options = [];
+		for (let year = startYear; year <= endYear; year++) {
+			options.push(<option key={year} value={year}>{year}</option>);
+		}
+		return options;
+	};
+
 	return (
 		<div className="h-screen flex flex-row justify-start bg-slate-100 dark:bg-dark_mode_bg">
 			<div className="flex-1">
 				<div className="flex-1 mx-auto px-5 py-5 bg-slate-100 dark:bg-dark_mode_bg">
-					{/* <div className="p-4 mb-4 bg-white rounded-lg shadow-lg">
-						<div className="text-center font-bold p-5">
-							<h1>Budget Expediture/ Year</h1>
+					<div className="p-4 mb-4 bg-white rounded-lg shadow-lg">
+						<div className="text-center">
+							<h1 className="font-bold p-5 text-[20px]">Budget Expenditure/ Faculty/ School/ Unit</h1>
+							<div>
+								<label htmlFor="month">Month: </label>
+								<select
+									className="h-full mr-2 rounded-md border bg-white border-gray-400 mb-5 text-gray-700 py-1 px-2 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 text-sm lg:text-base dark:border-[#484E51] dark:bg-dark_mode_card dark:text-dark_text2"
+									id="month"
+									value={selectedMonth}
+									onChange={handleMonthChangeExpenditure}
+								>
+									<option value="01">January</option>
+									<option value="02">February</option>
+									<option value="03">March</option>
+									<option value="04">April</option>
+									<option value="05">May</option>
+									<option value="06">June</option>
+									<option value="07">July</option>
+									<option value="08">August</option>
+									<option value="09">September</option>
+									<option value="10">October</option>
+									<option value="11">November</option>
+									<option value="12">December</option>
+								</select>
+								<label htmlFor="year">Year: </label>
+								<select
+									className="h-full rounded-md border mr-2 bg-white border-gray-400 mb-5 text-gray-700 py-1 px-2 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 text-sm lg:text-base dark:border-[#484E51] dark:bg-dark_mode_card dark:text-dark_text2"
+									id="year"
+									value={selectedYear}
+									onChange={handleYearChangeExpenditure}
+								>
+									{/* You can dynamically generate the years you want. The first parameter is the min year and the second parameter is the max year. */}
+									{generateYearOptions(2023, 2033)}
+								</select>
+							</div>
 						</div>
-						<LineGraphExpenditure1 />
-					</div> */}
-					<div className="p-4 mb-4 bg-white rounded-lg shadow-lg dark:bg-dark_mode_card">
-						<div className="text-center font-bold p-5 text-slate-800 dark:text-dark_text">
-							<h1>Number of Attendees each Faculty/ Unit by Date</h1>
+						<ExpenditureGraph
+							selectedMonth={parseInt(selectedMonth, 10)}
+							selectedYear={parseInt(selectedYear, 10)}
+							onTotalGrandTotalChange={handleTotalGrandTotalChange}
+						/>
+						<div className="text-center p-5 text-[20px] font-bold">
+							<h2>Total Expenditure for {getMonthText(selectedMonth)}, {selectedYear}: RM{totalGrandTotal}</h2>
+						</div>
+					</div>
 
+
+					<div className="p-4 mb-4 bg-white rounded-lg shadow-lg dark:bg-dark_mode_card">
+						<div className="text-center font-bold p-5 text-slate-800 dark:text-dark_text text-[20px]">
+							<h1>Number of Attendees each Faculty/ Unit by Date</h1>
 						</div>
 						<div className="justify-center items-center flex text-slate-800 dark:text-dark_text">
 							<div className="">
---
+++ components/analytics/ExpenditureGraph.tsx
@@ -0,0 +1,144 @@
+"use client"
+
+import React, { useEffect, useRef, useState } from "react";
+import Chart from "chart.js/auto";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+
+interface ExpenditureGraphProps {
+    selectedMonth: number;
+    selectedYear: number;
+    onTotalGrandTotalChange: (total: number) => void;
+}
+
+const ExpenditureGraph: React.FC<ExpenditureGraphProps> = ({ selectedMonth, selectedYear, onTotalGrandTotalChange }) => {
+    const chartRef = useRef<HTMLCanvasElement | null>(null);
+    const chartInstance = useRef<Chart | null>(null);
+    const supabase = createClientComponentClient();
+
+    const [monthlyData, setMonthlyData] = useState<{ faculty: string; total: number; }[]>([]);
+    const [totalGrandTotal, setTotalGrandTotal] = useState<number>(0);
+
+    // Create a start date as the 1st day of the selected month and year
+    const startDate = new Date(selectedYear, selectedMonth - 1, 1, 0, 0, 0);
+
+    // Create an end date as the last day of the selected month and year
+    const endDate = new Date(selectedYear, selectedMonth, 0, 23, 59, 59);
+
+    // function generateRandomColors(count: number) {
+    //     const colors = [];
+    //     for (let i = 0; i < count; i++) {
+    //         const color = `rgba(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, 0.7)`;
+    //         colors.push(color);
+    //     }
+    //     return colors;
+    // }
+
+    useEffect(() => {
+        // Calculate the total grand total
+        const total = monthlyData.reduce((acc, entry) => acc + entry.total, 0);
+        setTotalGrandTotal(total);
+    }, [monthlyData]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const { data, error } = await supabase
+                .from("external_forms")
+                .select("grand_total_fees, created_at, faculty")
+                .gte('created_at', startDate.toISOString())  // Filter for dates greater than or equal to start date
+                .lte('created_at', endDate.toISOString());
+
+            console.log(data);
+
+            if (error) {
+                console.error("Error fetching data:", error);
+                return;
+            }
+
+            const distinctFaculties = Array.from(new Set(data.map(item => item.faculty)));
+
+            // const facultyColors = generateRandomColors(Object.keys(distinctFaculties).length);
+
+            const monthlyData = distinctFaculties.map(faculty => ({
+                faculty: faculty,
+                total: 0,
+            }));
+
+            data.forEach((item) => {
+                const date = new Date(item.created_at);
+                const month = date.getMonth();
+                const facultyIndex = monthlyData.findIndex((entry) => {
+                    // console.log("Item Faculty:", item.faculty.trim().toLowerCase());
+                    // console.log("Entry Faculty:", entry.faculty.trim().toLowerCase());
+                    return entry.faculty.trim().toLowerCase() === item.faculty.trim().toLowerCase();
+                });
+                console.log("test" + facultyIndex)
+
+                if (facultyIndex !== -1 && !isNaN(month)) {
+                    monthlyData[facultyIndex].total += item.grand_total_fees;
+                }
+            });
+
+            setMonthlyData(monthlyData);
+
+            const calculatedTotalGrandTotal = monthlyData.reduce((acc, entry) => acc + entry.total, 0);
+            onTotalGrandTotalChange(calculatedTotalGrandTotal);
+
+            const ctx = chartRef.current?.getContext("2d");
+
+            if (ctx) {
+                if (chartInstance.current) {
+                    chartInstance.current.destroy();
+                }
+
+                chartInstance.current = new Chart(ctx, {
+                    type: "bar",
+                    data: {
+                        labels: monthlyData.map((entry) => entry.faculty),
+                        datasets: [
+                            {
+                                label: "Monthly Grand Total",
+                                data: monthlyData.map((entry) => entry.total),
+                                backgroundColor: "Red",
+                                borderWidth: 2,
+                            },
+                        ],
+                    },
+                    options: {
+                        responsive: true,
+                        maintainAspectRatio: false,
+                        scales: {
+                            x: {
+                                title: {
+                                    display: true,
+                                    text: "Faculty/ School/ Unit",
+                                },
+                                grid: {
+                                    display: false,
+                                },
+                            },
+                            y: {
+                                title: {
+                                    display: true,
+                                    text: "Total Grand Total",
+                                },
+                                grid: {
+                                    display: false,
+                                },
+                            },
+                        },
+                    },
+                });
+            }
+        }
+
+        fetchData();
+    }, [selectedMonth, selectedYear]);
+
+    return (
+        <div>
+            <canvas ref={chartRef} width="400" height="400"></canvas>
+        </div>
+    );
+};
+
+export default ExpenditureGraph;
