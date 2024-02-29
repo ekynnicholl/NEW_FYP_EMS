c34c6658e9cd07297666d8fd755da538bcc78b63
Ekyn Nicholl <76748552+ekynnicholl@users.noreply.github.com>
2024-02-29 11:50:27 +0800

    test2

--- app/form/[sub_id]/page.tsx
+++ app/form/[sub_id]/page.tsx
@@ -802,7 +802,7 @@ export default function AttendanceForm() {
 								Cancel
 							</button>
 						)}
-						{userType === 'visitor' ? (
+						{/* {userType === 'visitor' ? (
 							<button
 								type="submit"
 								className={`${info.attFormsStaffName ? 'bg-slate-900' : 'bg-gray-400'} text-white font-bold py-[11px] lg:py-3 px-8 mb-10 rounded focus:ring-2 focus:ring-offset-2 focus:ring-slate-900 text-sm lg:text-base`}
@@ -826,10 +826,10 @@ export default function AttendanceForm() {
 								disabled={!info.attFormsStaffName || !info.attFormsStaffID || !info.attFormsFacultyUnit}>
 								Submit
 							</button>
-						)}
+						)} */}
 
 
-						{/* <form onSubmit={handleSubmit}>
+						<form onSubmit={handleSubmit}>
 							{userType === 'visitor' ? (
 								<button
 									type="submit"
@@ -845,7 +845,7 @@ export default function AttendanceForm() {
 									Submit
 								</button>
 							)}
-						</form> */}
+						</form>
 
 
 
