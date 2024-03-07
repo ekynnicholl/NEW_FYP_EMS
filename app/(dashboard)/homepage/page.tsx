d9710b4c28adda734d1ab6d5155b345bf64669c3
Ekyn Nicholl <76748552+ekynnicholl@users.noreply.github.com>
2024-03-07 16:48:14 +0800

    added image upload design

--- app/(dashboard)/homepage/page.tsx
+++ app/(dashboard)/homepage/page.tsx
@@ -1295,8 +1295,28 @@ export default function Homepage() {
 
 								<hr className="border-t-2 border-slate-200 my-4 w-[270px] lg:w-[505px] dark:border-[#253345]" />
 
+								<p className="text-[11px] lg:text-[14px] text-mb-7 mb-[2px] font-normal text-slate-500 ml-[2px] dark:text-dark_textbox_title">
+									Event / Course Banner
+									<span className="text-[12px] lg:text-[14px] text-red-500 dark:text-red-600 ml-[2px]">
+										*
+									</span>
+								</p>
+
+								<div className="flex items-center justify-center w-full">
+									<label className="flex flex-col items-center justify-center w-full mr-2 h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+										<div className="flex flex-col items-center justify-center pt-5 pb-6">
+											<svg className="w-7 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+												<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
+											</svg>
+											<p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
+											<p className="text-xs text-gray-500 dark:text-gray-400">PNG, JPG (600x300px)</p>
+										</div>
+										<input id="dropzone-file" type="file" className="hidden" />
+									</label>
+								</div>
+
 								<div className="w-full pr-[11px]">
-									<p className="text-[11px] lg:text-[14px] text-mb-7 mb-[2px] font-normal text-slate-500 ml-[2px] -mt-1 lg:mt-0 dark:text-dark_textbox_title">
+									<p className="text-[11px] lg:text-[14px] text-mb-7 mb-[2px] font-normal text-slate-500 ml-[2px] mt-2 dark:text-dark_textbox_title">
 										Event / Course Name
 										<span className="text-[12px] lg:text-[14px] text-red-500 dark:text-red-600 ml-[2px]">
 											*
