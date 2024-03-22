f24943eb6e2396433343451c136e75ddc64dec23
Ekyn Nicholl <76748552+ekynnicholl@users.noreply.github.com>
2024-03-22 20:42:39 +0800

    half done ntf data fetch

--- components/view_ntf/ntf_list.tsx
+++ components/view_ntf/ntf_list.tsx
@@ -22,6 +22,12 @@ interface Form {
 
     full_name: string;
     staff_id: string;
+    course: string;
+    faculty: string;
+    transport: string;
+    travelling: string;
+    other_members: string;
+
 }
 
 const NTFList: React.FC<NTFListProps> = ({ atIdentifier }) => {
@@ -34,6 +40,12 @@ const NTFList: React.FC<NTFListProps> = ({ atIdentifier }) => {
     const [email, setEmail] = useState<string>("");
     const [full_name, setFullName] = useState<string>("");
     const [staff_id, setStaffID] = useState<string>("");
+    const [course, setCourse] = useState<string>("");
+    const [faculty, setFaculty] = useState<string>("");
+    const [transport, setTransport] = useState<string>("");
+    const [travelling, setTravelling] = useState<string>("");
+    const [other_members, setOtherMembers] = useState<string>("");
+
 
     const handleViewForm = (formId: string) => {
         setSelectedFormId(formId);
@@ -56,12 +68,12 @@ const NTFList: React.FC<NTFListProps> = ({ atIdentifier }) => {
                     const { data, error } = await (isEmail
                         ? supabase
                             .from('external_forms')
-                            .select('id, email, program_title, commencement_date, completion_date, organiser, venue, formStage, full_name, staff_id')
+                            .select('id, email, program_title, commencement_date, completion_date, organiser, venue, formStage, full_name, staff_id, course, faculty, transport, travelling, other_members, total_members')
                             .eq('email', searchQuery)
                             .order('formStage')
                         : supabase
                             .from('external_forms')
-                            .select('id, email, program_title, commencement_date, completion_date, organiser, venue, formStage, full_name, staff_id')
+                            .select('id, email, program_title, commencement_date, completion_date, organiser, venue, formStage, full_name, staff_id, course, faculty, transport, travelling, other_members, total_members')
                             .eq('staff_id', searchQuery)
                             .order('formStage'));
 
@@ -82,6 +94,11 @@ const NTFList: React.FC<NTFListProps> = ({ atIdentifier }) => {
                         setEmail(firstFormData.email);
                         setFullName(firstFormData.full_name);
                         setStaffID(firstFormData.staff_id);
+                        setCourse(firstFormData.course);
+                        setFaculty(firstFormData.faculty);
+                        setTransport(firstFormData.transport);
+                        setTravelling(firstFormData.travelling);
+                        setOtherMembers(firstFormData.other_members);
                     }
                 }
             } catch (e) {
@@ -99,20 +116,70 @@ const NTFList: React.FC<NTFListProps> = ({ atIdentifier }) => {
 
                 <div>
                     <div className="mx-auto max-w-6xl px-8 my-8 mt-6 mb-[200px]">
+                        <div className="ml-10">
+                            <div className="flex ml-[13px]">
+                                <div>
+                                    <Image
+                                        src="/swinburne_logo.png"
+                                        alt=""
+                                        width={200}
+                                        height={300}
+                                    />
+                                </div>
+                                <div className="ml-8 mt-2">
+                                    <p className="font-medium">Human Resources</p>
+                                    <h1 className="text-3xl font-bold text-slate-800 mb-4 mt-4 -ml-[1px]">
+                                        Nomination / Travelling Application Form
+                                    </h1>
+                                </div>
+                            </div>
+
+                            <div className="mb-4 text-slate-800 mt-2">
+                                <p className="mb-2">
+                                    <span className="text-[12px] lg:text-[14px] text-red-500 ml-[2px] mr-[6px]">
+                                        *
+                                    </span>
+                                    <span>
+                                        Before completing this form, please refer to the separate
+                                        document on “General Instructions for completing Nomination /
+                                        Travelling Application Form”, which is available on
+                                        SharePoint.
+                                    </span>
+                                </p>
+                                <p className="mb-2">
+                                    <span className="text-[12px] lg:text-[14px] text-red-500 ml-[2px] mr-[6px]">
+                                        *
+                                    </span>
+                                    <span>
+                                        All fields are mandatory to complete as required for each
+                                        applicable section.
+                                    </span>
+                                </p>
+                                <p>
+                                    <span className="text-[12px] lg:text-[14px] text-red-500 ml-[2px] mr-[6px]">
+                                        *
+                                    </span>
+                                    <span>
+                                        This form is also to be used for any contracted individual as
+                                        consultant, and is to be completed where applicable.
+                                    </span>
+                                </p>
+                            </div>
+                        </div>
+
+                        <hr className="mt-8" />
 
                         <div className="flex justify-between">
-
                             <form className="mt-6 w-full ml-[100px] mx-24">
-
                                 <div>
                                     <div>
                                         <a href="#personal_details" className="text-2xl font-bold mb-5 block text-slate-900">
                                             1. Personal Details {
-                                                        selectedFormId
-                                                            ? forms.find(form => form.id === selectedFormId)?.id || ""
-                                                            : id
-                                                    }
-                                                    
+                                                selectedFormId
+                                                    ? forms.find(form => form.id === selectedFormId)?.id || ""
+                                                    : id
+                                            }
+
                                         </a>
                                         <div>
                                             <p className="text-sm text-slate-800 font-medium ml-[1px]">Email
@@ -182,6 +249,121 @@ const NTFList: React.FC<NTFListProps> = ({ atIdentifier }) => {
                                             </div>
                                         </div>
                                     </div>
+
+                                    <div className="grid grid-cols-2 gap-5">
+                                        <div>
+                                            <p className="text-sm text-slate-800 font-medium mt-5 ml-[1px]">Designation / Course
+                                                <span className="text-[12px] lg:text-[14px] text-red-500 ml-[2px]">
+                                                    *
+                                                </span>
+                                            </p>
+                                            <div>
+                                                <input
+                                                    type="text"
+                                                    id="full_name"
+                                                    placeholder="Designation / Course"
+                                                    className="border border-gray-300 px-2 py-[7px] w-full rounded mt-2 placeholder-gray-500 lg:text-sm focus:outline-none focus:border-gray-400 focus:bg-white mb-[3px] text-[12px] text-left pl-[11px]"
+                                                    value={
+                                                        selectedFormId
+                                                            ? forms.find(form => form.id === selectedFormId)?.course || ""
+                                                            : course
+                                                    }
+                                                    readOnly // make the input read-only
+                                                />
+                                            </div>
+                                        </div>
+
+                                        <div>
+                                            <p className="text-sm text-slate-800 font-medium mt-5 ml-[1px]">Faculty / School / Unit
+                                                <span className="text-[12px] lg:text-[14px] text-red-500 ml-[2px]">
+                                                    *
+                                                </span>
+                                            </p>
+                                            <div>
+                                                <input
+                                                    type="text"
+                                                    id="staff_id"
+                                                    placeholder="Faculty / School / Unit"
+                                                    className="border border-gray-300 px-2 py-[7px] w-full rounded mt-2 placeholder-gray-500 lg:text-sm focus:outline-none focus:border-gray-400 focus:bg-white mb-[3px] text-[12px] text-left pl-[11px]"
+                                                    value={
+                                                        selectedFormId
+                                                            ? forms.find(form => form.id === selectedFormId)?.faculty || ""
+                                                            : faculty
+                                                    }
+                                                    readOnly // make the input read-only
+                                                />
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div className="grid grid-cols-2 gap-5">
+                                        <div>
+                                            <p className="text-sm text-slate-800 font-medium mt-5 ml-[1px]">
+                                                Type of Transportation{" "}
+                                                <span className="text-[12px] lg:text-[14px] text-red-500 ml-[2px]">*</span>
+                                            </p>
+                                            <div>
+                                                <input
+                                                    type="text"
+                                                    id="transportation"
+                                                    className="border border-gray-300 px-2 py-[7px] w-full rounded mt-2 placeholder-gray-500 lg:text-sm focus:outline-none focus:border-gray-400 focus:bg-white mb-[3px] text-[12px] text-left pl-[6px] hover:bg-slate-100 text-slate-800"
+                                                    value={
+                                                        selectedFormId
+                                                            ? forms.find(form => form.id === selectedFormId)?.transport || ""
+                                                            : transport
+                                                    }
+                                                    readOnly
+                                                />
+                                            </div>
+                                        </div>
+
+                                        <div>
+                                            <p className="text-sm text-slate-800 font-medium mt-5 ml-[1px]">Traveling in
+                                                <span className="text-[12px] lg:text-[14px] text-red-500 ml-[2px]">
+                                                    *
+                                                </span>
+                                            </p>
+                                            <div>
+                                                <input
+                                                    id="traveling"
+                                                    className="border border-gray-300 px-2 py-[7px] w-full rounded mt-2 placeholder-gray-500 lg:text-sm focus:outline-none focus:border-gray-400 focus:bg-white mb-[3px] text-[12px] text-left pl-[10px]  hover:bg-slate-100 text-slate-800"
+                                                    value={
+                                                        selectedFormId
+                                                            ? forms.find(form => form.id === selectedFormId)?.travelling || ""
+                                                            : travelling
+                                                    }
+                                                    readOnly
+                                                />
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div>
+                                        <p className="text-sm text-slate-800 font-medium mt-5 ml-[1px]">Name of other staff / student traveling together in group
+                                            <span className="text-[12px] lg:text-[14px] text-red-500 ml-[2px]">
+                                                *
+                                            </span>
+                                        </p>
+                                        <div>
+                                            <input
+                                                type="text"
+                                                id="full_name"
+                                                placeholder="Name of other staff"
+                                                className="border border-gray-300 px-2 py-[7px] w-full rounded mt-2 placeholder-gray-500 lg:text-sm focus:outline-none focus:border-gray-400 focus:bg-white mb-[3px] text-[12px] text-left pl-[11px]"
+                                                value={
+                                                    selectedFormId
+                                                        ? forms.find(form => form.id === selectedFormId)?.other_members || ""
+                                                        : other_members
+                                                }
+                                                readOnly
+                                            />
+                                        </div>
+                                    </div>
+
+
+
+
+
 
 
 
