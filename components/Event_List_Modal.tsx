98560f36453d5241b5685133b20f54d79fcc7418
Vvyy2 <102769646@students.swinburne.edu.my>
2024-04-01 01:20:18 +0800

    -
    -

--- components/Event_List_Modal.tsx
+++ components/Event_List_Modal.tsx
@@ -20,8 +20,8 @@ const EventListModal: React.FC<EventListModalProps> = ({ isVisible, onClose, chi
       id="wrapper"
       onClick={handleClose}>
       
-      <div className="border-slate-350 bg-slate-50 border-[1px] rounded-t-lg w-[500px] h-[600px] lg:w-[1200px] lg:h-[500px] flex flex-col relative shadow-md">
-      <div className="p-2 w-full bg-red-500 rounded-t-lg md:w-[1200px]"></div>
+      <div className="border-slate-350 bg-slate-50 border-[1px] rounded-t-lg w-[500px] h-[600px] lg:w-11/12 lg:h-11/12 flex flex-col relative shadow-md">
+      <div className="p-2 bg-red-500 rounded-t-lg w-full"></div>
         <button
           className="absolute top-5 right-3"
           onClick={() => onClose()}>
