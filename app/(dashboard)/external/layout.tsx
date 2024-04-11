19e8de0d1d8e06b609134ffcad70f9a99dcdcedc
HonWiCong <102761891@students.swinburne.edu.my>
2024-04-11 21:00:23 +0800

    added tab to external table page

--- app/(dashboard)/external/layout.tsx
+++ app/(dashboard)/external/layout.tsx
@@ -1,7 +1,13 @@
+import Link from "next/link";
+
 export default function ExternalPageLayout({ children }: { children: React.ReactNode }) {
 	return (
-		<div>
+		<>
+			<div className="flex gap-1 px-5 pt-4">
+				<Link className="px-3 py-2 rounded-md border bg-white" href="/external">Tab 1</Link>
+				<Link className="px-3 py-2 rounded-md border bg-white" href="/external/test">Tab 2</Link>
+			</div>
 			<div>{children}</div>
-		</div>
+		</>
 	);
 }
---
+++ app/(dashboard)/external/test/page.tsx
@@ -0,0 +1,8 @@
+export default function Test() {
+	return (
+		<>
+			<h1>Test</h1>
+			<p>This is a test page.</p>
+		</>
+	);
+}
