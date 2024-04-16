a1f5045416e924c50bf11f3d9a3937095da2ac4d
Ekyn Nicholl <76748552+ekynnicholl@users.noreply.github.com>
2024-04-16 22:33:28 +0800

    Update page.tsx

--- app/(dashboard)/homepage/page.tsx
+++ app/(dashboard)/homepage/page.tsx
@@ -2692,7 +2692,7 @@ export default function Homepage() {
 				</div>
 
 				<div>
-					<canvas ref={chartRef} style={{ height: '30px'}} className="mt-2"></canvas>
+					<canvas ref={chartRef} style={{ height: '20px'}} className="mt-2"></canvas>
 				</div>
 			</div>
 
