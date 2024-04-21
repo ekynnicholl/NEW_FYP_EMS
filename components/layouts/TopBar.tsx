9e878775c98078f56d532389f1b15f7c97c142b3
Ekyn Nicholl <76748552+ekynnicholl@users.noreply.github.com>
2024-04-21 16:45:55 +0800

    Update TopBar.tsx

--- components/layouts/TopBar.tsx
+++ components/layouts/TopBar.tsx
@@ -370,7 +370,7 @@ const TopBar: React.FC<TopBarProps> = ({ onViewModeChange, onIsDarkModeChange })
 										ID
 									</th>
 									<th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-										Email Address
+										Registered Email Address
 									</th>
 									<th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
 										Created At
