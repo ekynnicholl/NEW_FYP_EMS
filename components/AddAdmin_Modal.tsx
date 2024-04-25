e45bfc9c8b24e9d1f0ab1b8e8ebe3dfea53a3f2e
Ekyn Nicholl <76748552+ekynnicholl@users.noreply.github.com>
2024-04-26 01:35:12 +0800

    change from text to button

--- components/AddAdmin_Modal.tsx
+++ components/AddAdmin_Modal.tsx
@@ -11,10 +11,10 @@ const Modal: React.FC<ModalProps> = ({ isVisible, onClose, children }) => {
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center z-[999]" id="wrapper">
-      <div className="lg:w-[1170px] flex flex-col relative p-10 lg:p-0 dark:bg-dark_mode_card">
+      <div className="lg:w-[1250px] flex flex-col relative p-10 lg:p-0 dark:bg-dark_mode_card">
         <div>
           <button className="text-slate-500 text-xl md:text-xl lg:text-2xl absolute top-1 right-3 hover:font-medium lg:inline mt-10 lg:mt-0 mr-[53px] lg:mr-1 dark:text-dark_textbox_title" onClick={() => onClose()}>x</button>
-          <div className="bg-white p-2 rounded lg:rounded-lg border border-slate-200 shadow-md h-[550px] lg:h-[460px] overflow-y-hidden overflow-x-hidden dark:bg-dark_mode_card dark:border-[#253345]">
+          <div className="bg-white p-2 rounded lg:rounded-lg border border-slate-200 shadow-md h-[550px] lg:h-[540px] overflow-y-hidden overflow-x-hidden dark:bg-dark_mode_card dark:border-[#253345]">
             {children}
           </div>
         </div>
