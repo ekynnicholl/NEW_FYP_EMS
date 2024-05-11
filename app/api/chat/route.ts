bbb80a73bd937044f2d2294b757b63c3744ba195
Pinkslushie <110762227+Pinkslushie@users.noreply.github.com>
2024-05-11 18:49:35 +0800

    -
    -

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -4,23 +4,23 @@ import { OpenAIStream, StreamingTextResponse } from "ai";
 export const runtime = 'edge';
 
 const config = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY
+    apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY
 })
 const openai = new OpenAIApi(config);
 
-export async function POST(request: Request){
-    const {messages} = await request.json();
+export async function POST(request: Request) {
+    const { messages } = await request.json();
 
     const response = await openai.createChatCompletion({
         model: 'gpt-3.5-turbo',
         stream: true,
         messages: [
-            { role: "system", content: "University events"},
+            { role: "system", content: "University events" },
             ...messages
         ]
     })
 
     const stream = await OpenAIStream(response);
-    
+
     return new StreamingTextResponse(stream);
 }
