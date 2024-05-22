a0b9df7ea912ba4280f8812b7a94ad5778502f0d
Pinkslushie <110762227+Pinkslushie@users.noreply.github.com>
2024-05-22 14:55:20 +0800

    -
    -

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,39 +1,26 @@
 import { Configuration, OpenAIApi } from "openai-edge";
 import { OpenAIStream, StreamingTextResponse } from "ai";
-import { NextResponse } from "next/server";
-const Groq = require("groq-sdk");
 
 export const runtime = 'edge';
 
-// const config = new Configuration({
-//     apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY
-// })
-// const openai = new OpenAIApi(config);
+const config = new Configuration({
+    apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY
+})
+const openai = new OpenAIApi(config);
 
-const groq = new Groq({
-    apiKey: process.env.NEXT_PUBLIC_GROQ_API_KEY
-});
+export async function POST(request: Request) {
+    const { messages } = await request.json();
 
-export async function POST(request: { json: () => PromiseLike<{ messages: any; }> | { messages: any; }; }) {
-    try {
-        const { messages } = await request.json();
-
-        const chatCompletion = await getGroqChatCompletion(messages);
-
-        return NextResponse.json(chatCompletion);
-    } catch (error) {
-        console.error(error);
-        return NextResponse.error();
-    }
-}
-
-async function getGroqChatCompletion(messages: any) {
-    const response = await groq.chat.completions.create({
+    const response = await openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        stream: true,
         messages: [
             { role: "system", content: "University events" },
             ...messages
-        ],
-        model: "llama3-8b-8192"
-    });
-    return response;
+        ]
+    })
+
+    const stream = await OpenAIStream(response);
+
+    return new StreamingTextResponse(stream);
 }
